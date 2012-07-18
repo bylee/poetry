85e7d7bce0ce13625c22c1217c429639468d1b61
Bon-Yong Lee <bonyong.lee@samsung.com>
2012-07-18 19:41:23 +0900

    add change

---
+++ com.poetry/src/web/com/poetry/controller/SignController.java
@@ -0,0 +1,69 @@
+package com.poetry.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.poetry.model.SignStatus;
+import com.poetry.model.Poet;
+
+@Controller
+public class
+SignController
+{
+
+	@Autowired
+	@Qualifier( "authenticationManager" )
+	protected AuthenticationManager authenticationManager;
+
+	@RequestMapping( value = "/service/signstatus", method = RequestMethod.GET )
+	@ResponseBody
+	@SuppressWarnings("rawtypes")
+	public
+	SignStatus
+	getStatus()
+	{
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if ( null != auth && !auth.getName().equals("anonymousUser") && auth.isAuthenticated() )
+		{
+			return new SignStatus( auth.getName(), "success" );
+		} else {
+			return new SignStatus( null, "success" );
+		}
+	}
+
+	@RequestMapping( value = "/service/signin", method = RequestMethod.POST )
+	@ResponseBody
+	@SuppressWarnings("rawtypes")
+	public
+	SignStatus
+	login(
+		@RequestParam("j_username") String username,
+		@RequestParam("j_password") String password
+	)
+	{
+
+		final UsernamePasswordAuthenticationToken token =
+			new UsernamePasswordAuthenticationToken( username, password );
+		final Poet details = new Poet();
+		details.setUsername( username );
+		token.setDetails( details );
+
+		try {
+			final Authentication auth = authenticationManager.authenticate( token );
+			SecurityContextHolder.getContext().setAuthentication( auth );
+			return new SignStatus( auth.getName(), "success" );
+		} catch (BadCredentialsException e) {
+			return new SignStatus( username, "fail" );
+		}
+	}
+
+}
