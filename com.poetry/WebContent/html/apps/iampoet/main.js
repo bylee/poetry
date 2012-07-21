7236d33fe0c89784ac7f9f6b349aa598055e98e9
jonghyun kwon <playwant@gmail.com>
2012-07-21 11:01:13 +0900

    add splash

--- com.poetry/WebContent/html/apps/iampoet/main.js
+++ com.poetry/WebContent/html/apps/iampoet/main.js
@@ -19,6 +19,28 @@ $class('iampoet.MainController').extend(tau.ui.ParallelNavigator).define({
 		                     new iampoet.NewsFeedNavigator(),
 		                     new iampoet.SettingNavigator()
 		                     ]);
+		
+		this.appCtx = tau.getCurrentContext();
+    var tabs = this.appCtx.getConfig().tabs;
+		var tabBar = this.getTabBar();
+    var tabcomps = tabBar.getComponents();
+    for (i in tabs) {
+      var tabcomp = tabcomps[i];
+      var backImage = {
+        normal: tabs[i].icon,
+        selected: tabs[i].selectedIcon,
+        disabled: tabs[i].icon,
+        highlighted: tabs[i].icon,
+      };
+      tabcomp.setBackgroundImage(backImage);
+      tabcomp.setStyles({
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'top center'
+      });
+    }
+		
+		
+		
 	},
 	
 	destroy: function (){
